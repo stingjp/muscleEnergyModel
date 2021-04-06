52abf70088499e86737753e10b997816414ad4de
Jon Stingel <stingjp@stanford.edu>
2021-04-06 11:30:43 -0700

    script to plot activity and excitation for every muscle in the model. does individual subjects and then an average plot for all the subjects listed.

--- activityPlots.m
+++ activityPlots.m
@@ -21,7 +21,7 @@ cd(resultsdir)
 welkexoconditions = {'welkexo','welkexoexo'}; % ,'welknaturalslow','welknaturalnatural', ...
                   % 'welknaturalexo','welkexonatural','welkexoexo','welkexofast'};
 welknaturalconditions = {'welknatural','welknaturalnatural'};
-welksubjects = {'welk001'}; % welk002
+welksubjects = {'welk001','welk002'};
 
 thingstoplot = {'excitation','activation'};
 
@@ -29,11 +29,21 @@ load 'C:\Users\JP\code\repos\Stanford\delplab\projects\muscleModel\muscleEnergyM
 
 
 
-
+exomeans_excitation = struct();
+naturalmeans_excitation = struct();
+exomeans_activation = struct();
+naturalmeans_activation = struct();
+ 
 % loop through the subjects
 for subj=1:length(welksubjects)
     subject = char(welksubjects(subj));
     subjdir = strcat(resultsdir, strcat('/',subject));
+    
+    exomeans_excitation.(genvarname(subject)) = [];
+    naturalmeans_excitation.(genvarname(subject)) = [];
+    exomeans_activation.(genvarname(subject)) = [];
+    naturalmeans_activation.(genvarname(subject)) = [];
+    
     
     % loop through each of the things we want to plot
     for thing=1:length(thingstoplot)
@@ -242,10 +252,10 @@ for subj=1:length(welksubjects)
         for i=2:length(labels)
             subplot(7,8,i-1);
             templabel = char(labels(i));
-            muscleplot = welknaturalstruct.(genvarname(templabel));
-            plot(welknaturalstruct.time, muscleplot, ':')
+            muscleplot1 = welknaturalstruct.(genvarname(templabel));
+            plot(welknaturalstruct.time, muscleplot1, ':')
             hold on;
-            plot(welknaturalstruct.time, mean(muscleplot,2), 'k-', 'LineWidth', 2)
+            plot(welknaturalstruct.time, mean(muscleplot1,2), 'k-', 'LineWidth', 2)
             title(templabel)
             xlabel('% gait cycle')
             ylabel(tempthing)
@@ -256,7 +266,6 @@ for subj=1:length(welksubjects)
             '-dpng', '-r500')
         disp('print 1')
         
-        
         tempfig2 = figure('Position',[1,1,1920,1080]);
         % do more stuff
         % averaging and whatnot
@@ -264,10 +273,10 @@ for subj=1:length(welksubjects)
         for i=2:length(labels)
             subplot(7,8,i-1);
             templabel = char(labels(i));
-            muscleplot = welkexostruct.(genvarname(templabel));
-            plot(welkexostruct.time, muscleplot, ':')
+            muscleplot2 = welkexostruct.(genvarname(templabel));
+            plot(welkexostruct.time, muscleplot2, ':')
             hold on;
-            plot(welkexostruct.time, mean(muscleplot,2), 'k-', 'LineWidth', 2)
+            plot(welkexostruct.time, mean(muscleplot2,2), 'k-', 'LineWidth', 2)
             title(templabel)
             xlabel('% gait cycle')
             ylabel(tempthing)
@@ -278,6 +287,136 @@ for subj=1:length(welksubjects)
             '-dpng', '-r500')
         disp('print 2')
         
+        
+        % combined exo and natural fig
+        combineexonaturalfig = figure('Position',[1,1,1920,1080]);
+        title('red=natural, blue=exo');
+        % do more stuff
+        % averaging and whatnot
+        labels = fields(welkexostruct);
+        for i=2:length(labels)
+            subplot(7,8,i-1);
+            templabel = char(labels(i));
+            muscleplot2 = welkexostruct.(genvarname(templabel));
+            muscleplot1 = welknaturalstruct.(genvarname(templabel));
+            % plot(welkexostruct.time, muscleplot1, ':')
+            hold on;
+            plot(welkexostruct.time, mean(muscleplot2,2), 'b-', 'LineWidth', 2)
+            plot(welknaturalstruct.time, mean(muscleplot1,2), 'r-', 'LineWidth', 2)
+            title(templabel)
+            xlabel('% gait cycle')
+            ylabel(tempthing)
+            grid on;
+            % legend('exo','natural')
+            if tempthing == 'excitation'
+                exomeans_excitation.(genvarname(subject)) = [exomeans_excitation.(genvarname(subject)), mean(muscleplot2, 2)];
+                naturalmeans_excitation.(genvarname(subject)) = [naturalmeans_excitation.(genvarname(subject)), mean(muscleplot1, 2)];
+                excitelabels = fields(welkexostruct);
+                % exomeans_excitation. = [exomeans_excitation, mean(muscleplot2,2)];
+                % naturalmeans_excitation = [naturalmeans_excitation, mean(muscleplot1, 2)];
+            end
+            if tempthing == 'activation'
+                exomeans_activation.(genvarname(subject)) = [exomeans_activation.(genvarname(subject)), mean(muscleplot2, 2)];
+                naturalmeans_activation.(genvarname(subject)) = [naturalmeans_activation.(genvarname(subject)), mean(muscleplot1, 2)];
+                activelabels = fields(welkexostruct);
+                % exomeans_activation = [exomeans_activation, mean(muscleplot2, 2)];
+                % naturalmeans_activation = [naturalmeans_activation, mean(muscleplot1, 2)];
+            end
+        end
+        print(combineexonaturalfig, ...
+            strcat(strcat('C:\Users\JP\code\repos\Stanford\delplab\projects\muscleModel\analysis\', strcat(subject,'\')), strcat(strcat(tempthing, '_combined'), '.png')),...
+            '-dpng', '-r500')
+        disp('print combined')
+    end
+end
+
+
+
+% combined exo and natural fig - averages for subjects and mean
+subjectcombineexonaturalfig1 = figure('Position',[1,1,1920,1080]);
+title('red=natural, blue=exo');
+% do more stuff
+% averaging and whatnot
+    
+
+for i=1:length(excitelabels)-1    
+    % make a subplot
+    subplot(7,8,i);
+    templabel = char(excitelabels(i+1));
+    hold on;
+    tempsubjavgs1 = [];
+    tempsubjavgs2 = [];
+    
+    % for each muscle go through each subject
+    for subj=1:length(welksubjects)
+        subject = char(welksubjects(subj));
+        plot(welkexostruct.time, exomeans_excitation.(genvarname(subject))(:,i), 'b:', 'LineWidth', 1)
+        plot(welknaturalstruct.time, naturalmeans_excitation.(genvarname(subject))(:,i), 'r:', 'LineWidth', 1)
+        
+        % add each signal to the temp vector to get means before moving to
+        % the next muscle
+        tempsubjavgs1 = [tempsubjavgs1, naturalmeans_excitation.(genvarname(subject))(:,i)];
+        tempsubjavgs2 = [tempsubjavgs2, exomeans_excitation.(genvarname(subject))(:,i)];
     end
     
+    % now plot the means from the tempsubjavgs1/2
+    plot(welkexostruct.time, mean(tempsubjavgs2, 2), 'b-', 'LineWidth', 2)
+    plot(welknaturalstruct.time, mean(tempsubjavgs1, 2), 'r-', 'LineWidth', 2)
+    
+    
+    title(templabel)
+    xlabel('% gait cycle')
+    ylabel(tempthing)
+    grid on;
+    % legend('exo','natural')
 end
+
+print(subjectcombineexonaturalfig1, ...
+    strcat('C:\Users\JP\code\repos\Stanford\delplab\projects\muscleModel\analysis\', 'excitation_all_subjects_combined.png'),...
+    '-dpng', '-r500')
+disp('print combined')
+
+
+% now for the activations
+% combined exo and natural fig - averages for subjects and mean
+subjectcombineexonaturalfig2 = figure('Position',[1,1,1920,1080]);
+title('red=natural, blue=exo');
+% do more stuff
+% averaging and whatnot
+    
+for i=1:length(activelabels)-1   
+    % make a subplot
+    subplot(7,8,i);
+    templabel = char(activelabels(i+1));
+    hold on;
+    tempsubjavgs1 = [];
+    tempsubjavgs2 = [];
+    
+    % for each muscle go through each subject
+    for subj=1:length(welksubjects)
+        subject = char(welksubjects(subj));
+        plot(welkexostruct.time, exomeans_activation.(genvarname(subject))(:,i), 'b:', 'LineWidth', 1)
+        plot(welknaturalstruct.time, naturalmeans_activation.(genvarname(subject))(:,i), 'r:', 'LineWidth', 1)
+        
+        % add each signal to the temp vector to get means before moving to
+        % the next muscle
+        tempsubjavgs1 = [tempsubjavgs1, naturalmeans_activation.(genvarname(subject))(:,i)];
+        tempsubjavgs2 = [tempsubjavgs2, exomeans_activation.(genvarname(subject))(:,i)];
+    end
+    
+    % now plot the means from the tempsubjavgs1/2
+    plot(welkexostruct.time, mean(tempsubjavgs2, 2), 'b-', 'LineWidth', 2)
+    plot(welknaturalstruct.time, mean(tempsubjavgs1, 2), 'r-', 'LineWidth', 2)
+    
+    
+    title(templabel)
+    xlabel('% gait cycle')
+    ylabel(tempthing)
+    grid on;
+    % legend('exo','natural')
+end
+
+print(subjectcombineexonaturalfig2, ...
+    strcat('C:\Users\JP\code\repos\Stanford\delplab\projects\muscleModel\analysis\', 'activation_all_subjects_combined.png'),...
+    '-dpng', '-r500')
+disp('print combined')
