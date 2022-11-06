122dc423156859408a98060edda7ba8ccc1a1174
Jon Stingel <stingjp@stanford.edu>
2022-11-05 18:27:43 -0700

    working

--- runsubjects_python.py
+++ runsubjects_python.py
@@ -192,9 +192,10 @@ os.chdir(resultsbasedir)
 # subjects = ['welk002','welk003','welk005','welk007','welk008','welk009','welk010','welk013']
 # subjects = ['welk005','welk007','welk008']
 
-subjects = ['welk007']
-welkconditions = ['welkexo']
-trials = ['trial01'] # 
+subjects = ['welk013'] 
+welkconditions = ['welknatural'] 
+trials = ['trial01','trial02'] #,'trial02','trial03','trial04'] 
+
 command = "analyzeSubject_python"
 # command = "analyzeSubject_setup"
 
